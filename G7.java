7b5bf018687f69f392b08eb4fb757c84bdfaa94d
pavithraboopathi24 <32186708+pavithraboopathi24@users.noreply.github.com>
2018-06-21 17:28:34 +0530

    Create G7.java

---
+++ G7.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class G7
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.nextLine();
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(s1);
+        }
+    }
+}
